2b543917f244afe971d464f62677159e5c664a22
Matej <matejgaspert17@gmail.com>
2026-02-09 18:42:14 +0100

    navigation made responsive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
-﻿import { Component, signal } from '@angular/core';
+﻿// ...existing code...
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('super-awesome-page-V2');
+
+  menuOpen = signal(false);
+
+  constructor() {
+    document.addEventListener('click', (event) => {
+      console.log(this.menuOpen());
+      console.log('Clicked element:', event.target);
+      if(this.menuOpen() === true && !(event.target as HTMLElement).closest('.app-nav, .nav-toggle')) {
+        this.menuOpen.set(false);
+      }
+    });
+  }
+
+  toggleMenu() {
+    this.menuOpen.update(open => !open);
+  }
+
+  closeMenu() {
+    this.menuOpen.set(false);
+  }
+
 }
