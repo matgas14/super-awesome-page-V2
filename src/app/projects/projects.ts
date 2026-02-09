111600aca45562818e16675bc0f0c8529139f4fa
Matej <matejgaspert17@gmail.com>
2026-02-09 16:28:23 +0100

    projects done

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,11 +1,16 @@
+
+
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import projectsData from './projects-data.json';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
 export class Projects {
-
+  projects: any[] = projectsData;
 }
